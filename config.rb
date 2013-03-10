d29cf32bff70da298c2babf113dd33a5d09821f8
Robert Maynard <robert.maynard@kitware.com>
2013-03-10 11:43:30 -0400

    Initial commit of blog.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/DaxToolkit/"
+http_images_path = "/DaxToolkit/images"
+http_fonts_path = "/DaxToolkit/fonts"
+css_dir = "public/DaxToolkit/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
